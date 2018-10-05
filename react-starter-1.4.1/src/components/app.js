0ac6bd395cbab1b97457a0888bfc49e0d5afa230
Jeanette <aejlindberg@gmail.com>
2018-10-05 15:26:10 +0200

    First draft aboutMe

--- react-starter-1.4.1/src/components/app.js
+++ react-starter-1.4.1/src/components/app.js
@@ -4,8 +4,28 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        Find me in src/app.js!
+      <div className="wrapper">
+        <div className="aboutMe">
+          <div className="aboutMeImage">
+            <img src="me.png" alt="Jeanette K Lindberg" />
+          </div>
+          <div className="aboutMeText">
+            <h2>Jeanette Kalafatidis Lindberg</h2>
+            <h1>FRONTEND DEVELOPER</h1>
+            <p>Hello this is me! I am awsome and super duper mega smart and likes strawberries.</p>
+          </div>
+        </div>
+        <div className="aboutMeButton">
+          <button>SEE MY GITHUB</button>
+        </div>
+        <div className="techSection">
+          <div className="techText">
+            <p>480 coding hours - 50+ individual tasks - HTML5, CSS, SASS, Flexbox,
+             JavaScript, ES6, JSX, React, Node.js, Mongo DB, API:s - 6 sprints with
+             demos together with companies like Volumental, Bonnier, Comprend @ Technigo -
+             Exploring Machine Learning</p>
+          </div>
+        </div>
       </div>
     )
   }
