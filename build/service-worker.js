8d32aa21fd1ba4ebc9c07fcdd5b4e8a500a2f491
Jeanette <aejlindberg@gmail.com>
2018-10-19 13:48:56 +0200

    changed color on buttons

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2183428fcb35a49b0d9c07e0bec21997.js"
+  "/portfolio/precache-manifest.8772c9fcda937a12639df25d1dc725b4.js"
 );
 
 workbox.clientsClaim();
