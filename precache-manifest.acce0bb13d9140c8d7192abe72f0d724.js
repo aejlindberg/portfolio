88b41609edc9596d9dd6f553203b3792d376043e
Jeanette <aejlindberg@gmail.com>
2018-10-23 22:33:08 +0200

    Updates

--- precache-manifest.acce0bb13d9140c8d7192abe72f0d724.js
+++ precache-manifest.acce0bb13d9140c8d7192abe72f0d724.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "32fbe6087c735a48516a",
-    "url": "/portfolio/static/js/main.32fbe608.chunk.js"
+    "revision": "89893b86f7d73f41535f",
+    "url": "/portfolio/static/js/main.89893b86.chunk.js"
   },
   {
     "revision": "a203666c95ab4d8af47f",
     "url": "/portfolio/static/js/1.a203666c.chunk.js"
   },
   {
-    "revision": "32fbe6087c735a48516a",
-    "url": "/portfolio/static/css/main.d75ab4b8.chunk.css"
+    "revision": "89893b86f7d73f41535f",
+    "url": "/portfolio/static/css/main.e481a34e.chunk.css"
   },
   {
-    "revision": "8bae64cd92562e40d4ee3cb4a067bf41",
+    "revision": "db3cfc7c9282657244641634e06ee7b0",
     "url": "/portfolio/index.html"
   }
 ];
